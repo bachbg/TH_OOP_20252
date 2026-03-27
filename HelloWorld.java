26a1955baa07d8d1be6c2b44ec08b57e207e6291
lehongxn4-crypto <lehongxn4@gmail.com>
2026-03-27 14:04:16 +0700

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld
+{
+    public static void main(String args[])
+    {
+        System.out.printf("Xin chao cac ban\n");
+        System.out.printf("Hello World\n");
+    }
+}
