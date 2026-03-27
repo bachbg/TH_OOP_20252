a1895210517a22a24562463cea8460c95552829c
lehongxn4-crypto <lehongxn4@gmail.com>
2026-03-27 14:50:05 +0700

    Lab01

--- FirstDialog.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-public class FirstDialog
-{
-    public static void main(String args[])
-    {
-        J0ptionPane.showMessageDialog(null,"Hello world! How are you?");
-        System.exit(0);
-    }
-}
